5153f908a8c12228078678eb159458eb4ed46643
Jason <jasonhughes634@gmail.com>
2025-08-01 14:41:30 +0100

    Complete The Archive Theme

--- app.py
+++ app.py
@@ -16,18 +16,7 @@ from typing import List
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
 
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -307,5 +296,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5001)
 
